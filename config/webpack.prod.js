78dd61bf1cb8c33d284ebc58f9cde481d3172215
Ugi Ispoyo Widodo <ugi.widodo@dataon.com>
2022-11-14 01:04:59 +0700

    bug fix ssr

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,7 +6,6 @@ import commonConfig from "./webpack.common.js";
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
-console.log(__dirname)
 
 const prodConfig = {
     entry: {
